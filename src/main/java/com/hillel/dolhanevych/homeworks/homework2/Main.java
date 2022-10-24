3e95476dc764106a8e6ecbabefe962dd28b2fba0
Dolhanevych Andrii <dolhanevych@gmail.com>
2022-10-24 19:41:39 +0300

    commit all files

---
+++ src/main/java/com/hillel/dolhanevych/homeworks/homework2/Main.java
@@ -0,0 +1,18 @@
+package com.hillel.dolhanevych.homeworks.homework2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        double northernLatitude = 48.8582573;
+        double easternLongitude = 2.2945111;
+
+        System.out.print("Coordinates Eiffel Tower =  ");
+        System.out.print("Northern Latitude " + northernLatitude + "°");
+        System.out.print(" Eastern Longitude " + easternLongitude + "°");
+    }
+}
+
+
+
+
+
---
+++ src/main/java/com/hillel/dolhanevych/homeworks/homework3/Main.java
@@ -0,0 +1,7 @@
+package com.hillel.dolhanevych.homeworks.homework3;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}

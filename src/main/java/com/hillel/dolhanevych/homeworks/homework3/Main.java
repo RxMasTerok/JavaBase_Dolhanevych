b3a021254d5b6504bbddb1cfbf5f3fe255c5aab1
Dolhanevych Andrii <dolhanevych@gmail.com>
2022-10-25 15:11:26 +0300

    commit homework 3

--- src/main/java/com/hillel/dolhanevych/homeworks/homework3/Main.java
+++ src/main/java/com/hillel/dolhanevych/homeworks/homework3/Main.java
@@ -2,6 +2,8 @@ package com.hillel.dolhanevych.homeworks.homework3;
 
 public class Main {
     public static void main(String[] args) {
-
+        //github https://github.com/RxMasTerok/JavaBase_Dolhanevych
+        // test test test
+        // lol
     }
 }

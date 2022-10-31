860ac4bd591aa4a79b125702de9d3400d13a864d
Dolhanevych Andrii <dolhanevych@gmail.com>
2022-10-31 14:46:16 +0200

    commit homework 4(part two)

--- src/main/java/com/hillel/dolhanevych/homeworks/homework4/MainTwo.java
+++ src/main/java/com/hillel/dolhanevych/homeworks/homework4/MainTwo.java
@@ -7,15 +7,17 @@ public class MainTwo {
         int dynastyLeeWarriorAttack = 13;
         int dynastyLeeArcherAttack = 24;
         int dynastyLeeHorseManAttack = 46;
+        int countLee = 860;
 
 
         int dynastyMinWarriorAttack = 9;
         int dynastyMinArcherAttack = 35;
         int dynastyMinHorseManAttack = 12;
+        int countMin = (int) (860 * 1.5);
 
 
-        int allDynLeeAttack = (dynastyLeeWarriorAttack + dynastyLeeArcherAttack + dynastyLeeHorseManAttack) * 860;
-        int allDynMinAttack = (int) ((dynastyMinWarriorAttack + dynastyMinArcherAttack + dynastyMinHorseManAttack) * (860 * 1.5));
+        int allDynLeeAttack = (dynastyLeeWarriorAttack + dynastyLeeArcherAttack + dynastyLeeHorseManAttack) * countLee;
+        int allDynMinAttack = (int) ((dynastyMinWarriorAttack + dynastyMinArcherAttack + dynastyMinHorseManAttack) * countMin);
 
         System.out.println("Total Attack dynasty Lee: " +allDynLeeAttack);
         System.out.println("Total Attack dynasty Min: " +allDynMinAttack);
